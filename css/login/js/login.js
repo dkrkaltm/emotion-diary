7c81fc261d37a6d8eb4b27826574655a574cb9cd
dkrkaltm <88367623+dkrkaltm@users.noreply.github.com>
2022-04-18 17:58:08 +0900

    loginForm

---
+++ css/login/js/login.js
@@ -0,0 +1,12 @@
+let join = document.querySelector("#join");
+let IPF = document.querySelector("#ID-PW-find");
+let a;
+
+join.onclick = function(){
+
+    window.open(join.html,"join","width=500,height=500,top=200,left=600");
+};
+
+IPF.onclick = function(){
+    window.open(join.html,"find","width=500,height=500");
+};
