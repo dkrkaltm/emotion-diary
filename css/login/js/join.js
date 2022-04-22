6cac7c51658e8041f3e63f1a3622076801a7b81c
dkrkaltm <88367623+dkrkaltm@users.noreply.github.com>
2022-04-22 22:17:07 +0900

    joinForm(2) + findForm

--- css/login/js/join.js
+++ css/login/js/join.js
@@ -1,45 +1,85 @@
 //value
-let pattern1 = /[0-9]/; //숫자
-let pattern2 = /[a-zA-Z]/; //영어
-let pattern3 = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/; //한글
-let pattern4 = /[~!@#\#$%<>^&*]/; //특수문자
+let patternN = /[0-9]/; //숫자
+let patternE = /[a-zA-Z]/; //영어
+let patternH = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/; //한글
+let patternS = /[~!@#\#$%<>^&*]/; //특수문자
 let selectText;
 
 //id 중복검사용 
 let duplicateCheck ="abcd";
 
 //Object
-let selectForm ={};
+let inputPW={};
+let selectImf =
+{
+    //선택된 input
+    data:{},
+    // 입력 경고
+    warning(text){
+        labelImf.innerText = text; 
+        labelImf.style.color = 'red';
+       selectImf.data.style.color = 'red';
+       selectImf.data.style.borderBottom="solid 2px red";
+       selectImf.data.value= "";
+    },
+    // 입력 확인
+    confirmImf(){
+        labelImf.innerText ="";
+        selectImf.data.style.color = '';
+        selectImf.data.style.borderBottom="";
+    }
+};
 
+// 입력 검사 기준
+const pattern ={
+    N: /[0-9]/,//숫자
+    E:/[a-zA-Z]/,
+    H:/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/,
+    S:/[~!@#\#$%<>^&*]/
+};
 
 let joinForm = document.querySelector('.joinForm');
 let joinInput = joinForm.querySelectorAll('input');
-let labelId = joinForm.querySelector('label');
+let closeWindow = document.querySelector('.joinForm button:last-child');
 
 
+
+
+// 선택된 입력란 구분
 joinForm.onclick = function(){
-    selectForm = document.activeElement;
-    selectForm.onblur =function(){
-        
-        if(selectForm.placeholder==="아이디"){
-             if(pattern3.test(selectForm.value) || pattern4.test(selectForm.value)){
-                warning("*영어와 숫자만 입력 가능합니다");
-             }else if((duplicateCheck === selectForm.value)){
-                warning("*중복됩니다");     
+    selectImf.data = document.activeElement;
+    
+    labelImf = document.querySelector('label[for='+selectImf.data.id+']');
+    // 포커스 종료시
+    selectImf.data.onblur =function(){    
+        if(selectImf.data.placeholder==="아이디"){
+             if(pattern.H.test(selectImf.data.value) || pattern.S.test(selectImf.data.value)){
+                selectImf.warning("*영어와 숫자만 입력 가능합니다");
+             }else if((duplicateCheck === selectImf.data.value)){
+                selectImf.warning("*이미 아이디가 있습니다");     
              }else{
-                 labelId.innerText ="";
-                 selectForm.style.color = 'white';
-             }
-             
+                selectImf.confirmImf();
+             } 
+        }
+        else if(selectImf.data.placeholder === "비밀번호확인"){
+                inputPW = joinInput[1].value;
+                
+                console.log(inputPW);
+                if(inputPW != selectImf.data.value){
+                    selectImf.warning('비밀번호가 똑같지 않습니다');
+                }
+                else{
+                    selectImf.confirmImf();
+                }
+            
         }
     };
 };
+// 닫기 
+closeWindow.onclick = () => {
+    window.close();
+};
 
-function warning(text){
-    labelId.innerText = text; 
-    labelId.style.color = 'red';
-   selectForm.style.color = 'red';
-}
 
 
 
--- css/login/js/login.js
+++ css/login/js/login.js
@@ -1,10 +1,6 @@
 let join = document.querySelector("#join");
-let IPF = document.querySelector("#ID-PW-find");
 
 
 join.onclick = function(){
     window.open("./join.html","join","width=700,height=900,top=200,left=600");
-};
-IPF.onclick = function(){
-    window.open(join.html,"find","width=500,height=500");
 };
